dd1dbdf65829e874548e40d2ba9dbfd59556795b
Sudipto <sudipto.lahiry@gmail.com>
2018-01-14 12:39:02 +0800

    Added the controller

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userInTheMiddleOfTyping = false
+    
+    @IBAction func userSelectedNumber(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        if userInTheMiddleOfTyping {
+            
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+            
+        } else {
+            display.text = digit
+            userInTheMiddleOfTyping = true
+        }
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            return display.text = String(newValue)
+        }
     }
-
-
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userInTheMiddleOfTyping = false
+        }
+        
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+            
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    
 }
 
